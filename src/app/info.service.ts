25e37a0c5bd2a540c599b131f7cec5f836d6ba22
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-08-30 23:34:14 +0300

    adds a Http client Module

--- src/app/info.service.ts
+++ src/app/info.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoService {
 
-  constructor() { }
+  constructor(private http: HttpClient)  { }
 }
