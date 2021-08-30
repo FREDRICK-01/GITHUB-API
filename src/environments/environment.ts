197e646992d0613049a6f5a2b5c74407697deb13
FREDRICK-01 <fredrickgachuru73@gmail.com>
2021-08-30 23:50:22 +0300

    adds a GITHUB API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gitHubKey: "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"
 };
 
 /*
